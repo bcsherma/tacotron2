8cdbc645071fd6a1acc1251f90627a827cefba49
Ben Sherman <bcanfieldsherman@gmail.com>
2022-02-24 14:22:04 -0800

    Add simple data registration script

---
+++ register-data.py
@@ -0,0 +1,14 @@
+import wandb
+
+DATA_URL = "https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2"
+
+
+def register_data(url=DATA_URL):
+    run = wandb.init()
+    data_artifact = wandb.Artifact("lsj-tarball", type="raw data")
+    data_artifact.add_reference(url, name="tarball")
+    run.log_artifact(data_artifact)
+
+
+if __name__ == "__main__":
+    register_data()
